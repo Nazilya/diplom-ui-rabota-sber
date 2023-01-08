de2cad696bc0751e29771bb6ca62c63bb562e413
Nazilya <wkwad@mail.ru>
2023-01-08 23:03:00 +0500

    lesson14

--- src/test/java/sber/rabota/tests/BaseTest.java
+++ src/test/java/sber/rabota/tests/BaseTest.java
@@ -8,9 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import sber.rabota.helpers.Attach;
-
 import static com.codeborne.selenide.Configuration.baseUrl;
-
 import static com.codeborne.selenide.Selenide.open;
 
 
